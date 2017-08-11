ce0c84160641ae3b9efc166c36a2d16c6ecac1e2
Chauca <mc185249@ncr.com>
2017-08-11 09:58:31 -0300

    modificando router cliente

--- Socket_Cliente_digitalOcean/routes/cliente.js
+++ Socket_Cliente_digitalOcean/routes/cliente.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var io = require('socket.io-client');
 
-router.get("/", (req, res) => {
+router.get("/:userid", (req, res) => {
     var url = 'http://ncrapps.com:8080';
     var socket = io.connect(url);
-    var datos = { userid: req.body.userid }
+    var datos = { userid: req.params.userid }
     socket.on('connect', () => {
         socket.emit('setUserId', datos.userid);
         socket.emit('ClienteGet', datos);
