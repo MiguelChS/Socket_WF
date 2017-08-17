e89e86db06a430e136dd5c53e9e2a1428a5f9450
El Migue <miguel.sanchez528@gmail.com>
2017-08-17 15:57:37 -0300

    corriguiendo formularios json mal cargados

--- WF-Mails/Modulos/main.js
+++ WF-Mails/Modulos/main.js
@@ -93,7 +93,6 @@ function formateDataToOld(array) {
         let FormParse = {
             "formid": objForm.id,
             "txtfecha": objForm.data["date"],
-            "txtcsrcode": objForm.data["csrCode"],
             "csrcode": objForm.data["csrCode"],
             "enviado": !!form.enviado,
             "appversion": objForm.data["appversion"],
@@ -152,44 +151,152 @@ function formateDataToOld(array) {
                     "foto4": objForm.data["14"][3] ? objForm.data["14"][3] : null,
                     "txtparte": objForm.data["5"],
                     "txtcontacto": objForm.data["4"],
-                    "cliente": objForm.data["13"]
+                    "cliente": objForm.data["13"],
+
+                    "txtcsrcode": objForm.data["csrCode"]
                 }))
                 break;
             }
             case "2": {
                 formulario.cambiopidpad.push(Object.assign({}, FormParse, {
-                    "workorder":  objForm.data["21"],
-                    "custref":  objForm.data["22"],
-                    "sitename":  objForm.data["23"],
-                    "seriesaliente":  objForm.data["25"],
-                    "serieentrante":  objForm.data["26"],
+                    "workorder": objForm.data["21"],
+                    "custref": objForm.data["22"],
+                    "sitename": objForm.data["23"],
+                    "seriesaliente": objForm.data["25"],
+                    "serieentrante": objForm.data["26"],
                     "nropos": objForm.data["24"],
-                    "foto1": objForm.data["14"][0] ? objForm.data["14"][0] : null,
-                    "foto2": objForm.data["14"][1] ? objForm.data["14"][1] : null,
-                    "foto3": objForm.data["14"][2] ? objForm.data["14"][2] : null,
-                    "foto4": objForm.data["14"][3] ? objForm.data["14"][3] : null,
+                    "foto1": objForm.data["27"][0] ? objForm.data["27"][0] : null,
+                    "foto2": objForm.data["27"][1] ? objForm.data["27"][1] : null,
+                    "foto3": objForm.data["27"][2] ? objForm.data["27"][2] : null,
+                    "foto4": objForm.data["27"][3] ? objForm.data["27"][3] : null,
                 }))
                 break;
             }
             case "3": {
+                formulario.logistic.push(Object.assign({}, FormParse, {
+                    "txtwo": objForm.data["31"],
+                    "txtparte": objForm.data["32"],
+                    "txtreparador": objForm.data["35"],
+                    "txtretorno": objForm.data["34"],
+                    "txtcomentario": objForm.data["36"],
+                    "chkprofotos": !!objForm.data["37"].length,
+                    "foto1": objForm.data["37"][0] ? objForm.data["37"][0] : null,
+                    "foto2": objForm.data["37"][1] ? objForm.data["37"][1] : null,
+                    "foto3": objForm.data["37"][2] ? objForm.data["37"][2] : null,
+                    "foto4": objForm.data["37"][3] ? objForm.data["37"][3] : null,
+                    "cliente": null
+                }))
                 break;
             }
             case "4": {
+                formulario.memoriafiscal.push(Object.assign({}, FormParse, {
+                    "workorder": objForm.data["41"],
+                    "serie": objForm.data["42"],
+                    "idequipo": null,
+                    "cliente": objForm.data["43"],
+                    "custref": objForm.data["44"],
+                    "sitename": objForm.data["45"],
+                    "nropuntoventa": objForm.data["46"],
+                    "verfirmplacafiscal": objForm.data["47"],
+                    "serieimpresora": objForm.data["48"],
+                    "foto1": objForm.data["49"][0] ? objForm.data["49"][0] : null,
+                    "foto2": objForm.data["49"][1] ? objForm.data["49"][1] : null,
+                    "foto3": objForm.data["49"][2] ? objForm.data["49"][2] : null,
+                    "foto4": objForm.data["49"][3] ? objForm.data["49"][3] : null
+                }))
                 break;
             }
             case "5": {
+                formulario.mantenimiento.push(Object.assign({}, FormParse, {
+                    "txtwo": objForm.data["51"],
+                    "txtequipo": objForm.data["53"],
+                    "txtserial": objForm.data["52"],
+                    "txtfechamp": objForm.data["55"],
+                    "txtcomentario": objForm.data["56"],
+                    "chkprofotos": !!objForm.data["57"].length,
+                    "foto1": objForm.data["57"][0] ? objForm.data["57"][0] : null,
+                    "foto2": objForm.data["57"][1] ? objForm.data["57"][1] : null,
+                    "foto3": objForm.data["57"][2] ? objForm.data["57"][2] : null,
+                    "foto4": objForm.data["57"][3] ? objForm.data["57"][3] : null,
+                    "txtcsrcode": objForm.data["csrCode"],
+                    "cliente": objForm.data["54"],
+                    "selcliente": objForm.data["54"]
+                }))
                 break;
             }
             case "6": {
+                formulario.visitas.push(Object.assign({}, FormParse, {
+                    "txtwo": objForm.data["61"],
+                    "txtequipo": objForm.data["64"],
+                    "txtserial": objForm.data["63"],
+                    "txtfechavi": objForm.data["65"],
+                    "chkprofotos": !!objForm.data["66"].length,
+                    "foto1": objForm.data["66"][0] ? objForm.data["66"][0] : null,
+                    "foto2": objForm.data["66"][1] ? objForm.data["66"][1] : null,
+                    "foto3": objForm.data["66"][2] ? objForm.data["66"][2] : null,
+                    "foto4": objForm.data["66"][3] ? objForm.data["66"][3] : null,
+                    "txtcsrcode": objForm.data["csrCode"],
+                    "cliente": objForm.data["62"],
+                    "selcliente": objForm.data["62"]
+                }))
                 break;
             }
             case "7": {
+                formulario.tecladoencryptor.push(Object.assign({}, FormParse, {
+                    "workorder": objForm.data["71"],
+                    "serie": objForm.data["74"],
+                    "motivodanio": objForm.data["75"],
+                    "comentario": objForm.data["76"],
+                    "selcliente": objForm.data["72"],
+                    "equipo": objForm.data["73"],
+                    "cliente": objForm.data["72"],
+                    "foto1": null,
+                    "foto2": null,
+                    "foto3": null,
+                    "foto4": null
+                }))
                 break;
             }
             case "8": {
+                formulario.devolucionpartes.push(Object.assign({}, FormParse, {
+                    "warehouse": objForm.data["81"],
+                    "localidad": objForm.data["82"],
+                    "txtfecha": objForm.data["83"],
+
+                    "gng": objForm.data["85"].hasOwnProperty("guia"),
+                    "oca": objForm.data["85"].hasOwnProperty("ordenRetiro"),
+                    "guia": objForm.data["85"].hasOwnProperty("guia") ? objForm.data["85"]["guia"] : "",
+                    "empresa": objForm.data["85"].hasOwnProperty("guia") ? objForm.data["85"]["empresa"] : "",
+                    "ordenretiro": objForm.data["85"].hasOwnProperty("ordenRetiro") ? objForm.data["85"]["ordenRetiro"] : "",
+
+                    "foto1": objForm.data["87"][0] ? objForm.data["87"][0] : null,
+                    "foto2": objForm.data["87"][1] ? objForm.data["87"][1] : null,
+                    "foto3": objForm.data["87"][2] ? objForm.data["87"][2] : null,
+                    "foto4": objForm.data["87"][3] ? objForm.data["87"][3] : null,
+                    "guiadhl": objForm.data["84"],
+                    "cliente": null,
+                    "partes": objForm.data["86"].map(x => {
+                        return {
+                            "formid": objForm.id,
+                            "parte": x.nroParte,
+                            "descripcion": x.descripcion,
+                            "cantidad": x.cantidad,
+                            "estado": x.estado,
+                            "workorder": x.workOrde
+                        }
+                    })
+                }))
                 break;
             }
             case "9": {
+                formulario.recategorizacion.push(Object.assign({},FormParse,{
+                    "workorder":objForm.data["91"],
+                    "numeroEquipo":objForm.data["92"],
+                    "custRefNum":objForm.data["94"],
+                    "comentario":objForm.data["95"],
+                    "falla":objForm.data["96"],
+                    "cliente":objForm.data["93"]
+                }))
                 break;
             }
         }
